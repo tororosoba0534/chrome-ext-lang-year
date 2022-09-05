fa9a3d298e015cef2849e6815369dbee7487e7c4
tekuteku11udana <tekuteku11udana888@gmail.com>
2022-09-05 18:08:44 +0900

    Add DOM element in google search window.

---
+++ content.js
@@ -0,0 +1,7 @@
+let rootElm = document.getElementById("rcnt");
+
+let contentElm = document.createElement("div");
+contentElm.id = "ExtremeSearchRoot";
+contentElm.innerText = "Hello From Extension!!!";
+
+rootElm.append(contentElm);
