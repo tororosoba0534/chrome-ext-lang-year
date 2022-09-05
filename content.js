7e40e23e885e2feae20faba1d7191f920229a570
tekuteku11udana <tekuteku11udana888@gmail.com>
2022-09-05 18:26:55 +0900

    Add simply-styled button.

--- content.js
+++ content.js
@@ -2,6 +2,22 @@ let rootElm = document.getElementById("rcnt");
 
 let contentElm = document.createElement("div");
 contentElm.id = "ExtremeSearchRoot";
-contentElm.innerText = "Hello From Extension!!!";
+
+let EOne = document.createElement("button");
+let EAll = document.createElement("button");
+let JOne = document.createElement("button");
+let JAll = document.createElement("button");
+
+EOne.id = "ExtremeSearch-EOne";
+EAll.id = "ExtremeSearch-EAll";
+JOne.id = "ExtremeSearch-JOne";
+JAll.id = "ExtremeSearch-JAll";
+
+EOne.innerText = "EOne";
+EAll.innerText = "EAll";
+JOne.innerText = "JOne";
+JAll.innerText = "JAll";
+
+contentElm.append(EOne, EAll, JOne, JAll);
 
 rootElm.append(contentElm);
